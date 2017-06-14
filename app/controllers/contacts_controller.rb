632efca5084b305b1487b26022b6ff352d0f7f77
Timothee Garcia <grc.tmt@gmail.com>
2017-06-14 23:29:41 +0200

    update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,19 +4,20 @@ class ContactsController < ApplicationController
     contact = Contact.new(contact_params)
     # ContactMailer.contact.deliver_now
     client = Postmark::ApiClient.new(ENV['POSTMARK_API_KEY'])
-    client.deliver(
-      from: 'contact@nais-maquillage-permanent.fr',
-      to: 'contact@nais-maquillage-permanent.fr',
-      subject: 'Contact de nais-maquillage-permanent.fr',
-      html_body: "
-        <strong>CONTACT DE NAIS-MAQUILLAGE-PERMANENT.FR</strong><br><br>
-        <strong>Nom :</strong> #{contact.name}<br>
-        <strong>Email :</strong> #{contact.email}<br>
-        <strong>Message :</strong><br>
-        #{contact.message}<br>
-      ",
-      track_opens: true
-    )
+    p client
+    # client.deliver(
+    #   from: 'contact@nais-maquillage-permanent.fr',
+    #   to: 'contact@nais-maquillage-permanent.fr',
+    #   subject: 'Contact de nais-maquillage-permanent.fr',
+    #   html_body: "
+    #     <strong>CONTACT DE NAIS-MAQUILLAGE-PERMANENT.FR</strong><br><br>
+    #     <strong>Nom :</strong> #{contact.name}<br>
+    #     <strong>Email :</strong> #{contact.email}<br>
+    #     <strong>Message :</strong><br>
+    #     #{contact.message}<br>
+    #   ",
+    #   track_opens: true
+    # )
 
     redirect_to "/#contact"
   end
