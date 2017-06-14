56ca78106b8b0a846a1b8e610ca2ca8f7e73665d
Timothee Garcia <grc.tmt@gmail.com>
2017-06-14 22:11:59 +0200

    update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,8 +2,8 @@ class ContactsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :create ]
   def create
     Contact.new(contact_params)
-    # ContactMailer.contact.deliver_now
-    ContactMailer.contact
+    ContactMailer.contact.deliver_now
+    # ContactMailer.contact
     redirect_to "/#contact"
   end
 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,9 +2,17 @@ class ContactMailer < ApplicationMailer
   def contact
     # @contact = contact
 
+    # mail(
+    #   :to  => 'contact@nais-maquillage-permanent.fr',
+    #   :subject => 'Hello from Postmark'
+    # )
+
     mail(
+      :subject => 'Hello from Postmark',
       :to  => 'contact@nais-maquillage-permanent.fr',
-      :subject => 'Hello from Postmark'
+      :from => 'contact@nais-maquillage-permanent.fr',
+      :html_body => '<strong>Hello</strong> dear Postmark user.',
+      :track_opens => 'true'
     )
   end
 end
