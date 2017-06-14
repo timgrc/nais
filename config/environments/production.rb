a44ddb7209e8637fd30716180bbcf9a8cea346b3
Timothee Garcia <grc.tmt@gmail.com>
2017-06-14 23:41:22 +0200

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
-  # config.action_mailer.delivery_method     = :postmark
-  # config.action_mailer.postmark_settings   = {
-  #   api_key: ENV['POSTMARK_API_KEY'] }
-  # config.action_mailer.default_url_options = {
-  #   host: "nais-maquillage-permanent.fr" }
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = {
+    api_key: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = {
+    host: "nais-maquillage-permanent.fr" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
